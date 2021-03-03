b7b04634f83c430aea2e7ece7725230184794b6d
Stefano Carrazza <stefano.carrazza@cern.ch>
2021-03-03 21:46:09 +0100

    addin beta critical

---
+++ plot_beta_critical.py
@@ -0,0 +1,50 @@
+import argparse
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rc('text', usetex=True)
+plt.rc('text.latex', preamble=r'\usepackage{siunitx}')
+plt.rc('font', family='serif', size=10)
+plt.rc('pdf', compression=9)
+plt.rc('axes', labelsize='large')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", nargs='+', type=str)
+args = vars(parser.parse_args())
+
+
+def plot_band(axs, x, cv, std, label=None):
+    axs.plot(x, cv, '.-',label=label)
+    axs.fill_between(x, cv-std, cv+std, alpha=0.2)
+
+
+def main(filename):
+    """Main function for simulation.
+    """
+    odata = pd.DataFrame()
+    for f in filename:
+        odata = odata.append(pd.read_csv(f))
+
+    nqubits_range = np.sort(odata['nqubits'].unique())
+    cv = []
+    for nqubits in nqubits_range:
+        data = odata[odata['nqubits'] == nqubits]
+        beta_range = data['beta'].unique()
+        rmean = np.abs(np.array([ (1 - data[data['beta'] == b]['AA']/data[data['beta'] == b]['F_fit']).mean() for b in beta_range]))
+        cv.append(beta_range[np.argmin(rmean)])
+
+    title = str(odata["hamiltonian"].iloc[0]).replace('_', ' ') + ' - $\\beta_c$'
+
+    plt.title(title)
+    plt.plot(nqubits_range, cv, 'o--')
+    plt.xlabel('nqubits')
+
+    output = str(odata["hamiltonian"].iloc[0]) + '_betac.pdf'
+    print(f'Saving {output}')
+    plt.savefig(f"{output}", bbox_inches='tight')
+
+
+if __name__ == "__main__":
+    main(**args)
